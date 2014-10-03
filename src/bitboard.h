010c9b49ba4c1486be58c003eee85b861abe6bf6
Alain SAVARD <support@multicim.com>
2014-10-02 21:21:01 -0400

    KS_Corner2_8

--- src/bitboard.h
+++ src/bitboard.h
@@ -58,7 +58,7 @@ const Bitboard Rank8BB = Rank1BB << (8 * 7);
 
 //const Bitboard ContourBB = Rank1BB||Rank8BB||FileABB||FileHBB;
 const Bitboard CornersBB = SQ_A1||SQ_A8||SQ_H1||SQ_H8;
-const Bitboard InnerCornersBB = SQ_B1||SQ_B8||SQ_G1||SQ_G8;
+const Bitboard InnerCornersBB = SQ_A2||SQ_A7||SQ_H2||SQ_H7;
 
 
 CACHE_LINE_ALIGNMENT
