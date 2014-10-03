5e98d9cc5ef22471d372f13f3d52f2436edfa9c8
Alain SAVARD <support@multicim.com>
2014-10-02 20:30:25 -0400

    KS_Corner_2_2
    Take 4: this time, for exemple Kg8 and g7 protected only by the King.

--- src/bitboard.h
+++ src/bitboard.h
@@ -58,7 +58,7 @@ const Bitboard Rank8BB = Rank1BB << (8 * 7);
 
 //const Bitboard ContourBB = Rank1BB||Rank8BB||FileABB||FileHBB;
 const Bitboard CornersBB = SQ_B1||SQ_B8||SQ_G1||SQ_G8;
-const Bitboard InnerCornersBB = SQ_C2||SQ_C7||SQ_F2||SQ_F7;
+const Bitboard InnerCornersBB = SQ_B2||SQ_B7||SQ_G2||SQ_G7;
 
 CACHE_LINE_ALIGNMENT
 
