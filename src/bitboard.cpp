a1a2bb1bccefadcfd64e9e1e8f560c10c7995d16
Alain SAVARD <support@multicim.com>
2015-02-16 10:00:37 -0500

    B_FKD_20150216_V3
    Changed ArrowBB to be only on the vertical direction, so only the file
    batteries will have some bonus. Bench # 8103325

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -216,7 +216,7 @@ void Bitboards::init() {
           BetweenBB[s1][s2] = attacks_bb(pc, s2, SquareBB[s1]) & b;
           // b is xxxxx s1 xxxx s2 0 0 0 0 0
           // ArrowBB is 0 0 0 0 0
-          ArrowBB[s1][s2]   = LineBB[s1][s2] & ~(b | s1 | s2);
+          ArrowBB[s1][s2]   = LineBB[s1][s2] & ~(b | s1 | s2) & FileBB[s1];
       }
   }
 }
