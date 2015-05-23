f3507b140a9bb4b0a1a0afa3793701dcaf1d0e89
Alain SAVARD <support@multicim.com>
2015-05-23 07:05:10 -0400

    CCB_20150523
    Lower bonus
    Bench # 7555647
    Master#  8787152

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -408,12 +408,12 @@ namespace {
         // attacked and undefended squares around our king and the quality of
         // the pawn shelter (current 'score' value).
 
-        // * NEW * If king can castle on next move, this is considered a good defensive resource
+        // * NEW * If king can castle on next move, this gives more defensive options
 
         attackUnits =  std::min(74, ei.kingAttackersCount[Them] * ei.kingAttackersWeight[Them])
                      +  8 * ei.kingAdjacentZoneAttacksCount[Them]
                      + 25 * popcount<Max15>(undefended)
-					 - 10 * pos.legal_castle_count<Us>(ei.attackedBy[Them][ALL_PIECES])
+					 - 5 * pos.legal_castle_count<Us>(ei.attackedBy[Them][ALL_PIECES])
                      + 11 * (ei.pinnedPieces[Us] != 0)
                      - mg_value(score) / 8
                      - !pos.count<QUEEN>(Them) * 60;
