46d77c29afaf5b636fed615fea8f43529a5ddfad
Alain SAVARD <support@multicim.com>
2015-06-28 19:50:24 -0400

    OWR_20150628-5
    Last try
    Bench # 8306586

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -298,12 +298,12 @@ namespace {
         if (Pt == ROOK)
         {
             // Bonus for aligning with enemy pawns on the same rank/file
-            // but only if Rook has some horizontal mobility
-            if (relative_rank(Us, s) >= RANK_5 && (rank_bb(s) & b & mobilityArea[Us]))
+            if (relative_rank(Us, s) >= RANK_5)
             {
                 Bitboard alignedPawns = pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s];
+                // If the Rook has no horizontal mobility, score the bonus only once.
                 if (alignedPawns)
-                    score +=  popcount<Max15>(alignedPawns) * RookOnPawn;
+                    score +=  ((rank_bb(s) & b & mobilityArea[Us]) ? popcount<Max15>(alignedPawns) : 1) * RookOnPawn;
             }
 
             // Bonus when on an open or semi-open file
