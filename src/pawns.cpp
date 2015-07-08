baaa9095843aa5184bcd3a8ee8ab23af3694a8b2
Alain SAVARD <support@multicim.com>
2015-07-07 20:49:51 -0400

    AB_20150707_1
    If you can't tune them, simplify them !
    Bench #9287490

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -31,18 +31,12 @@ namespace {
   #define V Value
   #define S(mg, eg) make_score(mg, eg)
 
-  // Doubled pawn penalty by file
-  const Score Doubled[FILE_NB] = {
-    S(13, 43), S(20, 48), S(23, 48), S(23, 48),
-    S(23, 48), S(23, 48), S(20, 48), S(13, 43) };
-
-  // Isolated pawn penalty by opposed flag and file
-  const Score Isolated[2][FILE_NB] = {
-  { S(37, 45), S(54, 52), S(60, 52), S(60, 52),
-    S(60, 52), S(60, 52), S(54, 52), S(37, 45) },
-  { S(25, 30), S(36, 35), S(40, 35), S(40, 35),
-    S(40, 35), S(40, 35), S(36, 35), S(25, 30) } };
-
+  // Doubled pawn penalty
+  const Score Doubled = S(23, 48);
+    
+  // Isolated pawn penalty by opposed flag 
+  const Score Isolated[2] = {S(60, 52), S(40, 35)};
+  
   // Backward pawn penalty by opposed flag
   const Score Backward[2] = { S(67, 42), S(49, 24) };
 
@@ -176,7 +170,7 @@ namespace {
 
         // Score this pawn
         if (isolated)
-            score -= Isolated[opposed][f];
+            score -= Isolated[opposed];
 
         else if (backward)
             score -= Backward[opposed];
@@ -188,7 +182,7 @@ namespace {
             score += Connected[opposed][!!phalanx][more_than_one(supported)][relative_rank(Us, s)];
 
         if (doubled)
-            score -= Doubled[f] / distance<Rank>(s, frontmost_sq(Us, doubled));
+            score -= Doubled / distance<Rank>(s, frontmost_sq(Us, doubled));
 
         if (lever)
             score += Lever[relative_rank(Us, s)];
