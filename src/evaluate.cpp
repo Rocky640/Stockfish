c3cf357ef6175ff0a006f357defdb9c7741684ac
Alain SAVARD <support@multicim.com>
2015-08-16 08:45:12 -0400

    SK_20150815_2
    Back to exactly the same simple bonus as ongoing test, but only for
    Knight.
    Bench #  8373954

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -172,9 +172,7 @@ namespace {
   const Score Hanging            = S(31, 26);
   const Score PawnAttackThreat   = S(20, 20);
   const Score PawnSafePush       = S( 5,  5);
-  const Score KnightSafeMove1    = S(10,  0);
-  const Score KnightSafeMove2    = S(15,  0);
-
+  const Score KnightSafeMove     = S(10,  0);
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
@@ -300,9 +298,8 @@ namespace {
 
             if (Pt == KNIGHT)
             {
-                b &= ei.attackedBy[Us][PAWN] & ~ei.attackedBy[Them][PAWN] & ~pos.pieces();
-                if (b)
-                    score += more_than_one(b) ? KnightSafeMove2 : KnightSafeMove1;
+                if (b & ei.attackedBy[Us][PAWN] & ~ei.attackedBy[Them][PAWN] & ~pos.pieces())
+                    score += KnightSafeMove;
             }
             
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
