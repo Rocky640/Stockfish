9f25208b9e2d99042e738f84dc5124dec7f353f0
Alain SAVARD <support@multicim.com>
2015-09-27 23:42:28 -0400

    OPK_20150927_1
    @submit {When less pawns, the Knight is less effective}
    bench 7654316

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -179,6 +179,7 @@ namespace {
   const Score RookOnOpenFile     = S(43, 21);
   const Score RookOnSemiOpenFile = S(19, 10);
   const Score BishopPawns        = S( 8, 12);
+  const Score KnightPawns        = S(10, 10);
   const Score MinorBehindPawn    = S(16,  0);
   const Score TrappedRook        = S(92,  0);
   const Score Unstoppable        = S( 0, 20);
@@ -308,6 +309,10 @@ namespace {
             // Penalty for pawns on same color square of bishop
             if (Pt == BISHOP)
                 score -= BishopPawns * ei.pi->pawns_on_same_color_squares(Us, s);
+
+            // Adjust Knight value if more or less pawns on board
+            if (Pt == KNIGHT)
+                score += KnightPawns * (pos.count<PAWN>(Us) - 5);
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
             // pawn diagonally in front of it is a very serious problem, especially
