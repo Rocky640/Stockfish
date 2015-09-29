5fe0e90d7d5b6eaa84fee3def8e6a0a6640b1147
Alain SAVARD <support@multicim.com>
2015-09-28 20:25:24 -0400

    OK_20150928_1
    Take 1: S(0,20)
    @submit {When more pawns and less pieces, the Knight is more effective}
    bench: 9146090

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -179,7 +179,7 @@ namespace {
   const Score RookOnOpenFile     = S(43, 21);
   const Score RookOnSemiOpenFile = S(19, 10);
   const Score BishopPawns        = S( 8, 12);
-  const Score KnightPawns        = S(10, 10);
+  const Score KnightPawns        = S( 0, 10);
   const Score MinorBehindPawn    = S(16,  0);
   const Score TrappedRook        = S(92,  0);
   const Score Unstoppable        = S( 0, 20);
