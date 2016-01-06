985167587ef54f004172286500f2f016a7afc9f8
Alain SAVARD <support@multicim.com>
2016-01-06 13:51:12 -0500

    PDT_20160106_2
    Testing fishbench tuned values after 26355 iterations "as is"
    bench: 7955033
    master:8603285

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -283,13 +283,14 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   kingSquares[Us] = ksq;
   castlingRights[Us] = pos.can_castle(Us);
   int minKingPawnDistance = 0;
+  static int egvalue[] = {-2, -6, 15, 32, 49, 59, 71, 92, 104};
 
   Bitboard pawns = pos.pieces(Us, PAWN);
   if (pawns)
       while (!(DistanceRingBB[ksq][minKingPawnDistance++] & pawns)) {}
 
   if (relative_rank(Us, ksq) > RANK_4)
-      return make_score(0, -16 * minKingPawnDistance);
+      return make_score(0, -egvalue[minKingPawnDistance]);
 
   Value bonus = shelter_storm<Us>(pos, ksq);
 
@@ -300,7 +301,7 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
       bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
 
-  return make_score(bonus, -16 * minKingPawnDistance);
+  return make_score(bonus, -egvalue[minKingPawnDistance]);
 }
 
 // Explicit template instantiation
