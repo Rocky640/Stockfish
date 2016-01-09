5d1c29128198d8de9abf91794cb9d15aba46b74a
Alain SAVARD <support@multicim.com>
2016-01-08 21:15:16 -0500

    UA_20160108_1
    bench: 8476445
    master: 8603285

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -198,6 +198,7 @@ namespace {
   const Score Checked             = S(20, 20);
   const Score ThreatByHangingPawn = S(70, 63);
   const Score Hanging             = S(48, 28);
+  const Score UselessAttack       = S(20, 20);
   const Score ThreatByPawnPush    = S(31, 19);
   const Score Unstoppable         = S( 0, 20);
 
@@ -541,6 +542,14 @@ namespace {
         if (b)
             score += ThreatByKing[more_than_one(b)];
     }
+    
+    // Penalty when attacking pawn-defended pawns only with pieces.
+    b =   pos.pieces(Them, PAWN)
+        & ei.attackedBy[Them][PAWN]
+        & ei.attackedBy[Us][ALL_PIECES] 
+        & ~ei.attackedBy[Us][PAWN];
+    if (b)
+        score -= UselessAttack * popcount<Max15>(b);
 
     // Bonus if some pawns can safely push and attack an enemy piece
     b = pos.pieces(Us, PAWN) & ~TRank7BB;
