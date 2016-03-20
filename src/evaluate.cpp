dc2f74b046fd5aeba89b917bd711e858fc86d6aa
Alain SAVARD <support@multicim.com>
2016-03-20 19:07:05 -0400

    OOP_20160320_1
    Adjust attackUnits when pieces not defending our King and not attacking
    opponent.
    bench: 8284989
    master: 8075202

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -101,6 +101,7 @@ namespace {
     // a white knight on g5 and black's king is on g8, this white knight adds 2
     // to kingAdjacentZoneAttacksCount[WHITE].
     int kingAdjacentZoneAttacksCount[COLOR_NB];
+    int outOfPlay[COLOR_NB];
 
     Bitboard pinnedPieces[COLOR_NB];
     Material::Entry* me;
@@ -224,6 +225,7 @@ namespace {
     const Square Down = (Us == WHITE ? DELTA_S : DELTA_N);
 
     ei.pinnedPieces[Us] = pos.pinned_pieces(Us);
+    ei.outOfPlay[Us] = 0;
     Bitboard b = ei.attackedBy[Them][KING] = pos.attacks_from<KING>(pos.square<KING>(Them));
     ei.attackedBy[Them][ALL_PIECES] |= b;
     ei.attackedBy[Us][ALL_PIECES] |= ei.attackedBy[Us][PAWN] = ei.pi->pawn_attacks(Us);
@@ -279,6 +281,8 @@ namespace {
             if (bb)
                 ei.kingAdjacentZoneAttacksCount[Us] += popcount<Max15>(bb);
         }
+        else if (!(b & ei.kingRing[Us]))
+            ei.outOfPlay[Us] += 1;
 
         if (Pt == QUEEN)
             b &= ~(  ei.attackedBy[Them][KNIGHT]
@@ -398,6 +402,7 @@ namespace {
         // the pawn shelter (current 'score' value).
         attackUnits =  std::min(72, ei.kingAttackersCount[Them] * ei.kingAttackersWeight[Them])
                      +  9 * ei.kingAdjacentZoneAttacksCount[Them]
+                     +  6 * ei.outOfPlay[Us]
                      + 27 * popcount<Max15>(undefended)
                      + 11 * !!ei.pinnedPieces[Us]
                      - 64 * !pos.count<QUEEN>(Them)
