104506600c5bb5e8e38681bcdf817f50e41c14f0
Alain SAVARD <support@multicim.com>
2016-04-19 23:18:14 -0400

    NN1_20160419_1
    Do not consider null moves when only one minor and some pawns.
    bench: 7835335
    master: 7378965

--- src/search.cpp
+++ src/search.cpp
@@ -778,7 +778,7 @@ namespace {
     if (   !PvNode
         &&  depth >= 2 * ONE_PLY
         &&  eval >= beta
-        &&  pos.non_pawn_material(pos.side_to_move()))
+        &&  pos.non_pawn_material(pos.side_to_move()) > BishopValueMg)
     {
         ss->currentMove = MOVE_NULL;
         ss->counterMoves = nullptr;
