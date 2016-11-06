ee0b41fe754bd1531d9e7ca1ada6bbe09ded7dd6
Alain SAVARD <support@multicim.com>
2016-11-06 13:40:19 -0500

    PST_20161106
    2) Tuning the PawnStorm[0] (the "no friendly pawns" case)
    bench 4735038

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -53,16 +53,16 @@ namespace {
   };
 
   // Weakness of our pawn shelter in front of the king by [distance from edge][rank]
-  Value ShelterWeakness[][RANK_NB] = {
+  const Value ShelterWeakness[][RANK_NB] = {
     { V( 97), V(21), V(26), V(51), V(87), V( 89), V( 99) },
     { V(120), V( 0), V(28), V(76), V(88), V(103), V(104) },
     { V(101), V( 7), V(54), V(78), V(77), V( 92), V(101) },
     { V( 80), V(11), V(44), V(68), V(87), V( 90), V(119) }
   };
-  TUNE (SetRange(0, 150), ShelterWeakness);
 
+  // enum { NoFriendlyPawn, Unblocked, BlockedByPawn, BlockedByKing };
   // Danger of enemy pawns moving toward our king by [type][distance from edge][rank]
-  const Value StormDanger[][4][RANK_NB] = {
+  Value StormDanger[][4][RANK_NB] = {
     { { V( 0),  V(  67), V( 134), V(38), V(32) },
       { V( 0),  V(  57), V( 139), V(37), V(22) },
       { V( 0),  V(  43), V( 115), V(43), V(27) },
@@ -80,6 +80,9 @@ namespace {
       { V( 0),  V(  65), V( 142), V(48), V(32) },
       { V( 0),  V(  60), V( 126), V(51), V(19) } }
   };
+  
+  TUNE (SetRange(0, 200), StormDanger[0]);
+
 
   // Max bonus for king safety. Corresponds to start position with all the pawns
   // in front of the king and no enemy pawn on the horizon.
