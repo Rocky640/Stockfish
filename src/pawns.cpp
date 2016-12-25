bbb7333a50bb3bfbc383b1608fe60b70b734c9ed
Alain SAVARD <support@multicim.com>
2016-12-25 14:25:55 -0500

    SBT_20161225_2
    Fixed initialization
    bench: 5598926
    master: 5255881

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -106,7 +106,7 @@ namespace {
     Bitboard ourPawns   = pos.pieces(Us  , PAWN);
     Bitboard theirPawns = pos.pieces(Them, PAWN);
 
-    e->passedPawns[Us]   = e->pawnAttacksSpan[Us] = e->isoForward[Us];
+    e->passedPawns[Us]   = e->pawnAttacksSpan[Us] = e->isoForward[Us] = 0;
     e->semiopenFiles[Us] = 0xFF;
     e->kingSquares[Us]   = SQ_NONE;
     e->pawnAttacks[Us]   = shift<Right>(ourPawns) | shift<Left>(ourPawns);
