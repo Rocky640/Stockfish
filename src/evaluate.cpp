092d1fd1f72221a3c111ccedb17dbb459a7de045
Alain SAVARD <support@multicim.com>
2017-01-25 14:51:49 -0500

    ET_20170125_4
    Only for pieces which has other friendly pieces in their way
    bench 5918324
    master 6406285

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -310,7 +310,7 @@ namespace {
 
         int mob = popcount(b & ei.mobilityArea[Us]);
         mobility[Us] += MobilityBonus[Pt][mob];
-        ei.lowMobPieces[Us] += (mob <= 2 && !(b & (pos.pieces(Them) ^ pos.pieces(Them, PAWN))));
+        ei.lowMobPieces[Us] += (mob <= 2 && (b & (pos.pieces(Us) ^ pos.pieces(Us, PAWN))));
 
         if (Pt == BISHOP || Pt == KNIGHT)
         {
