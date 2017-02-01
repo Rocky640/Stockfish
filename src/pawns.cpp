4c31df2e49d5fb03655c154c76df3a7b069a369d
Alain SAVARD <support@multicim.com>
2017-01-31 20:16:32 -0500

    PLK_20170131_2
    take 2: higher penalty

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -291,7 +291,7 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
       bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
 
-  return make_score(bonus, -32 * minKingPawnDistance);
+  return make_score(bonus, -40 * minKingPawnDistance);
 }
 
 // Explicit template instantiation
