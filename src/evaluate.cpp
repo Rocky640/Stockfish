2237553d6921588f6ebba3d466f6b8115e12d796
Alain SAVARD <support@multicim.com>
2017-02-03 21:47:22 -0500

    PPK-20170203_1
    Simplify king bonus
    bench: 6046741
    master: 5885815

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -632,11 +632,7 @@ namespace {
 
             // Adjust bonus based on the king's proximity
             ebonus +=  distance(pos.square<KING>(Them), blockSq) * 5 * rr
-                     - distance(pos.square<KING>(Us  ), blockSq) * 2 * rr;
-
-            // If blockSq is not the queening square then consider also a second push
-            if (relative_rank(Us, blockSq) != RANK_8)
-                ebonus -= distance(pos.square<KING>(Us), blockSq + pawn_push(Us)) * rr;
+                     - distance(pos.square<KING>(Us  ), blockSq) * 3 * rr;
 
             // If the pawn is free to advance, then increase the bonus
             if (pos.empty(blockSq))
