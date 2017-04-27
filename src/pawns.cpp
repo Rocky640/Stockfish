e029ffeb3f9c7e150def0551d5488c1861931632
Alain SAVARD <support@multicim.com>
2017-04-27 15:29:10 -0400

    BR4_20170427_2
    Take 2
    bench: 6220408
    master: 6065528

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -139,7 +139,7 @@ namespace {
 
         // A pawn is backward when it is behind all pawns of the same color on the
         // adjacent files and cannot be safely advanced.
-        if (!neighbours || lever || relative_rank(Us, s) >= RANK_4 + (opposed | more_than_one(leverPush)))
+        if (!neighbours || lever || relative_rank(Us, s) >= (RANK_4 + (opposed | more_than_one(stoppers))))
             backward = false;
         else
         {
