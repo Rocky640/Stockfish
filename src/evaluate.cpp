0d8e486d51a1ddd1d98a32c60a81b4a0acdcb60d
Alain SAVARD <support@multicim.com>
2017-07-17 23:24:40 -0400

    PK_20170717_1
    Also consider unsafe threats by pawn push on the enemy king files.
    bench: 5666607
    master: 6258930

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -594,14 +594,18 @@ namespace {
         if (b)
             score += ThreatByKing[more_than_one(b)];
     }
+    
+    const Square ksq = pos.square<KING>(Us);
 
     // Find squares where our pawns can push on the next move
     b  = shift<Up>(pos.pieces(Us, PAWN)) & ~pos.pieces();
     b |= shift<Up>(b & TRank3BB) & ~pos.pieces();
 
-    // Keep only the squares which are not completely unsafe
-    b &= ~attackedBy[Them][PAWN]
-        & (attackedBy[Us][ALL_PIECES] | ~attackedBy[Them][ALL_PIECES]);
+    // Consider only the squares which are not completely unsafe, or which can
+    // lead to possible breaks on the king side.
+    b &=  (~attackedBy[Them][PAWN]
+        & (attackedBy[Us][ALL_PIECES] | ~attackedBy[Them][ALL_PIECES]))
+        | file_bb(ksq) | adjacent_files_bb(file_of(ksq));
 
     // Add a bonus for each new pawn threats from those squares
     b =  (shift<Left>(b) | shift<Right>(b))
