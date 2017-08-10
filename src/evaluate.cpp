a0c943ccd7ac8dba2373b38db2d12fa70513a99a
Alain SAVARD <support@multicim.com>
2017-08-10 19:24:06 -0400

    a2
    adjust only the kingAttackersCount and Weight
    5222393

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -319,7 +319,7 @@ namespace {
         {
             kingAttackersCount[Us]++;
             kingAttackersWeight[Us] += KingAttackWeights[Pt];
-            kingAdjacentZoneAttacksCount[Us] += popcount(bb & attackedBy[Them][KING]);
+            kingAdjacentZoneAttacksCount[Us] += popcount(b & attackedBy[Them][KING]);
         }
 
         int mob = popcount(b & mobilityArea[Us]);
