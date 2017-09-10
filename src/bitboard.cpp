972ea6b4ad2a7f93c267dc9681ceb759fabb8f54
Alain SAVARD <support@multicim.com>
2017-09-10 08:42:15 -0400

    KD1_20170910_1
    Tidy up

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -247,9 +247,6 @@ void Bitboards::init() {
           }
       }
   }
-  sync_cout << KnightDistance[SQ_A1][SQ_H8] << sync_endl; //6
-  sync_cout << KnightDistance[SQ_E4][SQ_H8] << sync_endl; //3
-  sync_cout << KnightDistance[SQ_B1][SQ_H8] << sync_endl; //5
 }
 
 
