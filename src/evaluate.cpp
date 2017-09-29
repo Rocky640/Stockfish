558a2e2d8148637a1af1ea4fe2778b40fffa5fb3
Alain SAVARD <support@multicim.com>
2017-09-29 19:26:22 -0400

    BC_20170929
    Simple bonus if small center has no pawns of same color as bishop
    bench: 4901875
    master:5037819

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -204,6 +204,7 @@ namespace {
   // Assorted bonuses and penalties used by evaluation
   const Score MinorBehindPawn     = S( 16,  0);
   const Score BishopPawns         = S(  8, 12);
+  const Score OpenCenter          = S( 16,  6);
   const Score RookOnPawn          = S(  8, 24);
   const Score TrappedRook         = S( 92,  0);
   const Score WeakQueen           = S( 50, 10);
@@ -338,9 +339,16 @@ namespace {
                 && (pos.pieces(PAWN) & (s + pawn_push(Us))))
                 score += MinorBehindPawn;
 
-            // Penalty for pawns on the same color square as the bishop
+            
             if (Pt == BISHOP)
+            {
+                // Penalty for pawns on the same color square as the bishop
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s);
+
+                // Bonus if the center squares are not occupied by pawns.
+                if (  !(pos.pieces(PAWN) & Center & (DarkSquares & s ? DarkSquares : ~DarkSquares)))
+                    score += OpenCenter;
+            }
 
             // An important Chess960 pattern: A cornered bishop blocked by a friendly
             // pawn diagonally in front of it is a very serious problem, especially
