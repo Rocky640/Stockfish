9fc286528e757d44e27eef0d5317678ef30f1388
Alain SAVARD <support@multicim.com>
2017-11-11 17:21:15 -0500

    LVB_20171111
    bench 5278511
    master 5109559

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -102,7 +102,7 @@ namespace {
     Bitboard b, neighbours, stoppers, doubled, supported, phalanx;
     Bitboard lever, leverPush;
     Square s;
-    bool opposed, backward;
+    bool opposed, backward, controlled;
     Score score = SCORE_ZERO;
     const Square* pl = pos.squares<PAWN>(Us);
 
@@ -135,6 +135,8 @@ namespace {
         neighbours = ourPawns   & adjacent_files_bb(f);
         phalanx    = neighbours & rank_bb(s);
         supported  = neighbours & rank_bb(s - Up);
+        controlled =    more_than_one(stoppers & PseudoAttacks[KNIGHT][s])
+                     && relative_rank(Us, s) < RANK_4;
 
         // A pawn is backward when it is behind all pawns of the same color on the
         // adjacent files and cannot be safely advanced.
@@ -173,6 +175,8 @@ namespace {
         }
 
         // Score this pawn
+	if (controlled) phalanx = 0;
+
         if (supported | phalanx)
             score += Connected[opposed][!!phalanx][popcount(supported)][relative_rank(Us, s)];
 
