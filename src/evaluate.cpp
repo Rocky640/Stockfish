584aecb5c4c8211a28fa95d4c45f03453569c94c
Alain SAVARD <support@multicim.com>
2017-12-09 19:03:01 -0500

    SC2_20171212_1
    Forgot to comment one line.
    bench:5247551
    master:

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -450,7 +450,7 @@ namespace {
         kingDanger =        kingAttackersCount[Them] * kingAttackersWeight[Them]
                     + 102 * kingAdjacentZoneAttacksCount[Them]
                     + 191 * popcount(kingRing[Us] & weak)
-                    + 143 * bool(pos.pinned_pieces(Us))
+                    //+ 143 * bool(pos.pinned_pieces(Us))
                     - 848 * !pos.count<QUEEN>(Them)
                     -   9 * mg_value(score) / 8
                     +  40;
