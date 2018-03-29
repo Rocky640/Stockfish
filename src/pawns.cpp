4047317a1bc9dfd66e4adf1fdd200e0475bd2be0
Alain SAVARD <support@multicim.com>
2018-03-29 07:26:12 -0400

    LSL_20180329_1
    If the only stoppers are levers, score as a passed pawn
    bench: 5274144
    master:  5326261

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -151,10 +151,10 @@ namespace {
         // not attacked more times than defended.
         if (   !(stoppers ^ lever ^ leverPush)
             && !(ourPawns & forward_file_bb(Us, s))
-            && popcount(supported) >= popcount(lever)
-            && popcount(phalanx)   >= popcount(leverPush))
+            && (   (   popcount(supported) >= popcount(lever)
+                    && popcount(phalanx)   >= popcount(leverPush))
+                || (stoppers == lever)))
             e->passedPawns[Us] |= s;
-
         else if (   stoppers == SquareBB[s + Up]
                  && relative_rank(Us, s) >= RANK_5)
         {
