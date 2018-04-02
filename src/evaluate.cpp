b5668ce642676d5a90895a759c85af7a33958009
Alain SAVARD <support@multicim.com>
2018-04-02 14:32:24 -0400

    IA_20180402_1
    bench: 5657510
    master 6036915

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -768,7 +768,7 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * outflanking
-                    +  8 * pe->pawn_asymmetry()
+                    +  8 * (pe->pawn_asymmetry() - 2)
                     + 12 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
