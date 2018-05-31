727b1959536d52a3411170750f613a73c9fa5282
Alain SAVARD <support@multicim.com>
2018-05-31 12:34:38 -0400

    PI2
    bench 4955544

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -770,9 +770,10 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * outflanking
                     +  8 * pe->pawn_asymmetry()
-                    +  2 * pe->pawn_distance() * pos.count<PAWN>()
+                    + 16 * pos.count<PAWN>()
+                    +  4 * std::min(pe->pawn_distance(), 5)
                     + 48 * !pos.non_pawn_material()
-                    -136;
+                    -140;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
