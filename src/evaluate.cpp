e1f6ad21fe7c417767ac3d9573b600a722b8d09d
Alain SAVARD <support@multicim.com>
2018-06-10 10:30:11 -0400

    XRAY2-2
    bench: 4948386

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -307,8 +307,10 @@ namespace {
 
     while ((s = *pl++) != SQ_NONE)
     {
-        // Find directly attacked squares
-        b = pos.attacks_from<Pt>(s);
+        // Find attacked squares, including x-ray attacks for bishops and rooks
+        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(QUEEN))
+          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK))
+                         : pos.attacks_from<Pt>(s);
 
         if (pos.blockers_for_king(Us) & s)
             b &= LineBB[pos.square<KING>(Us)][s];
