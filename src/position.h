be235db5d38319856b66d3af1037788c5a562e9d
Alain SAVARD <support@multicim.com>
2018-06-24 10:16:39 -0400

    PB
    Never prune pawn attacks
    bench:  4839467

--- src/position.h
+++ src/position.h
@@ -311,7 +311,8 @@ inline bool Position::pawn_passed(Color c, Square s) const {
 
 inline bool Position::advanced_pawn_push(Move m) const {
   return   type_of(moved_piece(m)) == PAWN
-        && relative_rank(sideToMove, from_sq(m)) > RANK_4;
+        && (   relative_rank(sideToMove, from_sq(m)) > RANK_4
+            || PawnAttacks[sideToMove][to_sq(m)] & pieces(~sideToMove));
 }
 
 inline Key Position::key() const {
