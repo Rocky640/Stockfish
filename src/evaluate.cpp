c5fca99826b102d20361d27e85072693f3c1b50c
Alain SAVARD <support@multicim.com>
2018-07-01 09:34:47 -0400

    BW_1
    Adjust blocking factor for bishops.
    
    White Nd5 Black Bb7 and d6
    master currently increase the blocking penalty even if d6 is not same color.
    
    here we do such adjustment only for pawn blockers such as
    White c4 d5 against Bb7 and d6
    
    bench 4891736

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -352,7 +352,10 @@ namespace {
             {
                 // Penalty according to number of pawns on the same color square as the
                 // bishop, bigger when the center files are blocked with pawns.
-                Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
+                Bitboard samecolor = (DarkSquares & s) ? DarkSquares : ~DarkSquares;
+                Bitboard blocked =   pos.pieces(Us, PAWN)
+                                   & shift<Down>(  (pos.pieces() & ~samecolor)
+                                                 | (pos.pieces(Them, PAWN) & attackedBy[Them][PAWN]));
 
                 score -= BishopPawns * pe->pawns_on_same_color_squares(Us, s)
                                      * (1 + popcount(blocked & CenterFiles));
