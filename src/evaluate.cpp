a1d6965514940340dc2ad29c9944851c714dcba3
Alain SAVARD <support@multicim.com>
2018-07-15 15:18:03 -0400

    RP_1
    rook protecting passed pawn from side or from behind
    bench: 4969434

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -376,8 +376,13 @@ namespace {
         if (Pt == ROOK)
         {
             // Bonus for aligning rook with enemy pawns on the same rank/file
+            // or protecting a passed pawn which is not behind.
             if (relative_rank(Us, s) >= RANK_5)
-                score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
+            {
+                bb  = pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s];
+                bb |= pe->passed_pawns(Us) & b & ~forward_file_bb(Them, s);
+                score += RookOnPawn * popcount(bb);
+            }
 
             // Bonus for rook on an open or semi-open file
             if (pe->semiopen_file(Us, file_of(s)))
