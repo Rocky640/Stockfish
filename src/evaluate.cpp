2d3c636471f4448ac80c28a8592c536f018f7a81
Alain SAVARD <support@multicim.com>
2018-07-23 11:54:08 -0400

    CO_1
    Consider also d3/e3 to be outpost squares for white (and d6/e6 for black)
    
    bench: 5100089

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -289,8 +289,9 @@ namespace {
 
     constexpr Color     Them = (Us == WHITE ? BLACK : WHITE);
     constexpr Direction Down = (Us == WHITE ? SOUTH : NORTH);
-    constexpr Bitboard OutpostRanks = (Us == WHITE ? Rank4BB | Rank5BB | Rank6BB
-                                                   : Rank5BB | Rank4BB | Rank3BB);
+    constexpr Bitboard OutpostRanks = 
+       (Us == WHITE ? Rank6BB | Rank5BB | Rank4BB | shift<SOUTH>(Center)
+                    : Rank3BB | Rank4BB | Rank5BB | shift<NORTH>(Center));
     const Square* pl = pos.squares<Pt>(Us);
 
     Bitboard b, bb;
