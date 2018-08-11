37b830054b18226c6ddeafde7ac83abf663d4cfe
Rocky640 <support@multicim.com>
2018-08-11 12:21:04 -0400

    Imb2
    Remove k2
    Same bench

--- src/material.cpp
+++ src/material.cpp
@@ -49,13 +49,13 @@ namespace {
     {  46,   39,  24,   -24,    0       }, // Rook
     {  97,  100, -42,   137,  268,    0 }  // Queen
   };
-  
+ 
   // Game stage bonuses - contains a bonus/malus, depending on the number of certain pieces
-  constexpr int GameStagePawns[]   = {  0, 23, 17, -8, -19, -24, -18, -1, 12 };
-  constexpr int GameStageKnights[] = {  0, 28, 48,  14 };
-  constexpr int GameStageBishops[] = {  0, 28, 42, -6 };
-  constexpr int GameStageRooks[]   = {  0, 20, 31, -11 };
-  constexpr int GameStageQueens[]  = {  0, 67, -15 };
+  constexpr int GameStagePawns[]   = {  0, 23-4, 17-8, -8-12, -19-16, -24-20, -18-24, -1-28, 12-32 }; //-4
+  constexpr int GameStageKnights[] = {  0, 28-12, 48-24,  14-36, 0-48 }; //-12
+  constexpr int GameStageBishops[] = {  0, 28-12, 42-24, -6-36, 0-48 }; //-12
+  constexpr int GameStageRooks[]   = {  0, 20-20, 31-40, -11-60,0-80 }; //-20
+  constexpr int GameStageQueens[]  = {  0, 67-40, -15-80, 0-120, 0-160 }; //-40
 
   // Endgame evaluation and scaling functions are accessed directly and not through
   // the function maps because they correspond to more than one material hash key.
@@ -96,16 +96,14 @@ namespace {
    // Game stage coefficient k1, being the sum of game stage bonuses
    int k1 = 0;
    k1 += GameStagePawns[std::min(pieceCount[Us][PAWN], 8)];
-   k1 += GameStageKnights[std::min(pieceCount[Us][KNIGHT], 3)];
-   k1 += GameStageBishops[std::min(pieceCount[Us][BISHOP], 3)];
-   k1 += GameStageRooks[std::min(pieceCount[Us][ROOK], 3)];
-   k1 += GameStageQueens[std::min(pieceCount[Us][QUEEN], 2)];
+   k1 += GameStageKnights[std::min(pieceCount[Us][KNIGHT], 4)];
+   k1 += GameStageBishops[std::min(pieceCount[Us][BISHOP], 4)];
+   k1 += GameStageRooks[std::min(pieceCount[Us][ROOK], 4)];
+   k1 += GameStageQueens[std::min(pieceCount[Us][QUEEN], 4)];
     
-  // Game stage coefficient k2, being the index of nominal game stage
-    int k2 = (pieceCount[Us][PAWN] + pieceCount[Us][KNIGHT] * 3 + pieceCount[Us][BISHOP] * 3 + pieceCount[Us][ROOK] * 5 + pieceCount[Us][QUEEN] * 10);
-  
+
   // Initial bonus
-    int bonus = (k1 - (k2 * 4)) * 16;
+    int bonus = k1 * 16;
     
     // Second-degree polynomial material imbalance, by Tord Romstad
     for (int pt1 = NO_PIECE_TYPE; pt1 <= QUEEN; ++pt1)
