1529ce8388b465af1976d046397db30b302098ad
Rocky640 <support@multicim.com>
2018-08-12 08:26:03 -0400

    MSC
    bench: 4689229
    
    Consider a Minor check safe if  supported by another piece but only defended by a major (the queen case was already in master this test adds the rook case)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -435,7 +435,7 @@ namespace {
         // Attacked squares defended at most once by our queen or king
         weak =  attackedBy[Them][ALL_PIECES]
               & ~attackedBy2[Us]
-              & (~attackedBy[Us][ALL_PIECES] | attackedBy[Us][KING] | attackedBy[Us][QUEEN]);
+              & (~attackedBy[Us][ALL_PIECES] | attackedBy[Us][KING] | attackedBy[Us][QUEEN] | attackedBy[Us][ROOK]);
 
         // Analyse the safe enemy's checks which are possible on next move
         safe  = ~pos.pieces(Them);
@@ -445,14 +445,14 @@ namespace {
         b2 = attacks_bb<BISHOP>(ksq, pos.pieces() ^ pos.pieces(Us, QUEEN));
 
         // Enemy queen safe checks
-        if ((b1 | b2) & attackedBy[Them][QUEEN] & safe & ~attackedBy[Us][QUEEN])
+        if ((b1 | b2) & attackedBy[Them][QUEEN] & safe & ~(attackedBy[Us][QUEEN] | attackedBy[Us][ROOK]))
             kingDanger += QueenSafeCheck;
 
         b1 &= attackedBy[Them][ROOK];
         b2 &= attackedBy[Them][BISHOP];
 
         // Enemy rooks checks
-        if (b1 & safe)
+        if (b1 & safe & ~(attackedBy[Us][ROOK]))
             kingDanger += RookSafeCheck;
         else
             unsafeChecks |= b1;
