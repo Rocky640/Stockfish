37440cab57a1131b6fb7dbd188942cbcdfc7c7ce
Rocky640 <support@multicim.com>
2018-08-29 13:28:10 -0400

    SR_1
    Space experiment # 1
    bench: 4886850

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,8 +723,8 @@ namespace {
 
     constexpr Color Them = (Us == WHITE ? BLACK : WHITE);
     constexpr Bitboard SpaceMask =
-      Us == WHITE ? CenterFiles & (Rank2BB | Rank3BB | Rank4BB)
-                  : CenterFiles & (Rank7BB | Rank6BB | Rank5BB);
+      Us == WHITE ? CenterFiles & (Rank2BB | Rank3BB)
+                  : CenterFiles & (Rank7BB | Rank6BB);
 
     // Find the available squares for our pieces inside the area defined by SpaceMask
     Bitboard safe =   SpaceMask
