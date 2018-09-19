606ff7a1261255fc41d019594267c457f4297823
Rocky640 <support@multicim.com>
2018-09-19 13:40:08 -0400

    BS2
    bench 4487772

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -736,7 +736,7 @@ namespace {
     behind |= (Us == WHITE ? behind >>  8 : behind <<  8);
     behind |= (Us == WHITE ? behind >> 16 : behind << 16);
 
-    int bonus = popcount(safe) + popcount(behind & safe);
+    int bonus = popcount(safe) + popcount(behind & safe & ~(Rank2BB | Rank7BB));
     int weight = pos.count<ALL_PIECES>(Us) - 2 * pe->open_files();
 
     Score score = make_score(bonus * weight * weight / 16, 0);
