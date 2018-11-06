bd27a53e8c1aa294a504465799e8a330f3738cad
Rocky640 <support@multicim.com>
2018-11-06 15:24:35 -0500

    KC3
    Larger penalty
    bench 3761080

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -477,7 +477,7 @@ namespace {
                      +  69 * kingAttacksCount[Them]
                      + 185 * popcount(kingRing[Us] & weak)
                      + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
-                     +  50 * bool(CenterFiles & ksq)
+                     + 100 * bool(CenterFiles & ksq)
                      +   4 * tropism
                      - 873 * !pos.count<QUEEN>(Them)
                      -   6 * mg_value(score) / 8
