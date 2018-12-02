658a1c044caa2fe18740a11d9c02d935e3f9b15d
Alain SAVARD <support@multicim.com>
2018-12-02 08:37:32 -0500

    VKS-2
    Try assigning KingRing only once.
    bench: 3057978

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -274,6 +274,7 @@ namespace {
             kingRing[Us] |= shift<EAST>(kingRing[Us]);
 
         kingAttackersCount[Them] = popcount(kingRing[Us] & pe->pawn_attacks(Them));
+        kingRing[Us] &= ~dble_attacks_bb<Us>(pos.pieces(Us, PAWN));
         kingAttacksCount[Them] = kingAttackersWeight[Them] = 0;
     }
   }
