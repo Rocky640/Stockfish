00b333c8dde9d2c0c2d9845689e48847dfd9595c
Alain SAVARD <support@multicim.com>
2019-01-13 18:58:10 -0500

    IS_1
    try a sharper gradient for weak pawns

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -32,9 +32,9 @@ namespace {
   #define S(mg, eg) make_score(mg, eg)
 
   // Pawn penalties
-  constexpr Score Backward = S( 9, 24);
+  constexpr Score Backward = S( 9-9, 24+9);
   constexpr Score Doubled  = S(11, 56);
-  constexpr Score Isolated = S( 5, 15);
+  constexpr Score Isolated = S( 5-5, 15+5);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
