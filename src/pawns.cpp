6d19370e8cb1f74698dbd6a419694fb6236a4dad
Alain SAVARD <support@multicim.com>
2019-01-27 15:09:05 -0500

    WU4
    bench 3951546

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -33,7 +33,7 @@ namespace {
 
   // Pawn penalties
   constexpr Score Backward = S( 9, 24);
-  constexpr Score Doubled  = S(11, 56);
+  constexpr Score Doubled  = S(11-11, 56-13);
   constexpr Score Isolated = S( 5, 15);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
@@ -138,7 +138,7 @@ namespace {
             score -= Backward, e->weakUnopposed[Us] += !opposed;
 
         if (doubled && !support)
-            score -= Doubled;
+            score -= Doubled,  e->weakUnopposed[Us] += !opposed;
     }
 
     return score;
