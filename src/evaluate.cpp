c2f37800f1e77f426d196b6d779ec8b5cddc116f
Alain SAVARD <support@multicim.com>
2019-03-16 08:31:13 -0400

    LRP_1
    Master exclude all low ranks pawns (or blocked pawns) from mobilityArea
    here we exlude only if close to king (or blocked pawns)
    bench 3478011

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -225,12 +225,13 @@ namespace {
     constexpr Color     Them = (Us == WHITE ? BLACK : WHITE);
     constexpr Direction Up   = (Us == WHITE ? NORTH : SOUTH);
     constexpr Direction Down = (Us == WHITE ? SOUTH : NORTH);
-    constexpr Bitboard LowRanks = (Us == WHITE ? Rank2BB | Rank3BB: Rank7BB | Rank6BB);
+    //constexpr Bitboard LowRanks = (Us == WHITE ? Rank2BB | Rank3BB: Rank7BB | Rank6BB);
 
     const Square ksq = pos.square<KING>(Us);
 
-    // Find our pawns that are blocked or on the first two ranks
-    Bitboard b = pos.pieces(Us, PAWN) & (shift<Down>(pos.pieces()) | LowRanks);
+    // Find our pawns that are blocked or near our king
+    Bitboard b =  pos.pieces(Us, PAWN)
+                & (shift<Down>(pos.pieces()) | DistanceRingBB[ksq][2]);
 
     // Squares occupied by those pawns, by our king or queen or controlled by
     // enemy pawns are excluded from the mobility area.
