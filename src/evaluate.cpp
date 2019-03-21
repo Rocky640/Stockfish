d2e67a22cc2d0e2f58e139301e84bf38c389c992
Alain SAVARD <support@multicim.com>
2019-03-20 20:12:04 -0400

    AS2
    a different way to remove unopposed pawn count in the asymetry: just
    double the passed pawn count.
    bench: 3579287

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -743,7 +743,7 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   9 * pe->pawn_asymmetry()
+    int complexity =  18 * pe->passed_pawn_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
@@ -776,7 +776,7 @@ namespace {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material(WHITE) == BishopValueMg
             && pos.non_pawn_material(BLACK) == BishopValueMg)
-            sf = 8 + 4 * pe->pawn_asymmetry();
+            sf = 8 + 8 * pe->passed_pawn_count();
         else
             sf = std::min(40 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide), sf);
 
