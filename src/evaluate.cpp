db33655c7859fcc2025a8f3a5bb589f1e39207c7
Alain SAVARD <support@multicim.com>
2019-03-30 07:03:31 -0400

    P2
    Consider squares occupied by own pieces (except king or pawn) as
    "defended"
    Consider squares occupied by own (doubled) pawns as "unsafe"
    bench 3259387

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -650,10 +650,10 @@ namespace {
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN) & pos.attacks_from<ROOK>(s);
 
                 if (!(pos.pieces(Us) & bb))
-                    defendedSquares &= attackedBy[Us][ALL_PIECES];
+                    defendedSquares &= attackedBy[Us][ALL_PIECES] | (pos.pieces(Us) ^ pos.pieces(Us, PAWN, KING));
 
                 if (!(pos.pieces(Them) & bb))
-                    unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
+                    unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them) | pos.pieces(Us, PAWN);
 
                 // If there aren't any enemy attacks, assign a big bonus. Otherwise
                 // assign a smaller bonus if the block square isn't attacked.
@@ -661,7 +661,7 @@ namespace {
 
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
-                if (defendedSquares & ~pos.pieces(Us, PAWN) == squaresToQueen)
+                if (defendedSquares == squaresToQueen)
                     k += 6;
 
                 else if (defendedSquares & blockSq)
