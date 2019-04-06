3c45710bca75a47592a768258fea1454c2bbc8be
Alain SAVARD <support@multicim.com>
2019-04-06 16:14:58 -0400

    KP_0_5
    bench: 3005568

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -550,6 +550,16 @@ namespace {
         score += Hanging * popcount(weak & b);
     }
 
+    // Bonus for knight which can safely add new attack to some enemy weak pawns
+    if (pos.pieces(Us, KNIGHT))
+    {
+        b = 0;
+        weak = pos.pieces(Them, PAWN) & ~stronglyProtected;
+        while (weak)
+            b |= pos.attacks_from<KNIGHT>(pop_lsb(&weak));
+        score += make_score(0, 5) * popcount(b & attackedBy[Us][KNIGHT] & ~(attackedBy[Them][ALL_PIECES] | pos.pieces(Us)));
+    }
+
     // Bonus for restricting their piece moves
     b =   attackedBy[Them][ALL_PIECES]
        & ~stronglyProtected
