39920015dfef803cad16b95d717bc6f195b548c3
Alain SAVARD <support@multicim.com>
2019-09-02 19:47:24 -0400

    SR4-2
    bench: 4095925

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -721,8 +721,8 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   8 * pe->pawn_assymetry()
-                    + 10 * pos.count<PAWN>()
+    int complexity =   9 * pe->pawn_assymetry()
+                    +  6 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
