30f1840a1a0f59e38a12e7d476444a9cac967b1d
Alain SAVARD <support@multicim.com>
2019-09-25 13:34:40 -0400

    SSA_3
    bnch 3825825

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -702,7 +702,7 @@ namespace {
     behind |= shift<Down+Down>(behind);
 
     int bonus = popcount(safe) + popcount(behind & safe);
-    int weight = pos.count<ALL_PIECES>(Us);
+    int weight = pos.count<ALL_PIECES>(Us) + 1;
     Score score = make_score(bonus * weight * weight / 16, 0);
 
     if (T)
