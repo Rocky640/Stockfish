372919780683b427e5c3334098bbaf599a7c01f5
Alain SAVARD <support@multicim.com>
2019-10-14 14:24:43 -0400

    BC3
    bench 3918404

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -317,7 +317,7 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
                 int weight = popcount(blocked & CenterFiles);
                 score -=   BishopPawns
-                         * clamp(pos.pawns_on_same_color_squares(Us, s), weight - 2, 6)
+                         * clamp(pos.pawns_on_same_color_squares(Us, s), weight - 2, 9 - weight)
                          * (weight + 1);
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
