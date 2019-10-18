ac1c2798dd9ff1be27f346d665aee2245247ce14
Alain SAVARD <support@multicim.com>
2019-10-17 20:48:58 -0400

    BC2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -317,8 +317,8 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 int factor =  pos.pawns_on_same_color_squares(Us, s)
-                            * (1 + popcount(blocked & CenterFiles));
-                score -= BishopPawns * std::min(factor, 17);
+                            * std::max(1, popcount(blocked & CenterFiles));
+                score -= BishopPawns * factor;
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
