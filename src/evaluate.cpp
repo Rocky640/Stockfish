904f3de8a7333c718f60febdcf4acb2647d94f64
Alain SAVARD <support@multicim.com>
2019-11-30 17:59:53 -0500

    K3a
    decrease further
    bench 5153468

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -602,8 +602,8 @@ namespace {
             Square blockSq = s + Up;
 
             // Adjust bonus based on the king's proximity
-            bonus += make_score(0, (  king_proximity(Them, blockSq) * 5
-                                    - king_proximity(Us,   blockSq) * 2) * w);
+            bonus += make_score(0, (  king_proximity(Them, blockSq) * 9
+                                    - king_proximity(Us,   blockSq) * 4) * w / 2);
 
             // If blockSq is not the queening square then consider also a second push
             if (r != RANK_7)
