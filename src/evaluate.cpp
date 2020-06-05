59fb833bef20abe53b83a8ca47babd28b26454a7
Alain SAVARD <support@multicim.com>
2020-06-04 22:13:50 -0400

    OM_1fix
    bench: 4778979

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -314,7 +314,7 @@ namespace {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
             if (bb & s)
-                score += mob * (Pt == KNIGHT) ? KnightOutpost : BishopOutpost;
+                score += ((Pt == KNIGHT) ? KnightOutpost : BishopOutpost) * mob;
             else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
                 score += ReachableOutpost;
 
