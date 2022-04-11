f8f319475a8d6002cd2d9fcb27d104353b846333
Rocky640 <algotek@hotmail.fr>
2022-04-10 22:48:23 -0400

    Update movepick.cpp
    lower move picker rank  for quiet moves by pinners
    bench: 8613192

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -144,7 +144,7 @@ void MovePicker::score() {
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   +     pos.pinners(us) & from_sq(m) ? 10000 : 0
+                   +     pos.pinners(us) & from_sq(m) ? -10000 : 0
                    +     (threatened & from_sq(m) ?
                            (type_of(pos.moved_piece(m)) == QUEEN && !(to_sq(m) & threatenedByRook)  ? 50000
                           : type_of(pos.moved_piece(m)) == ROOK  && !(to_sq(m) & threatenedByMinor) ? 25000
